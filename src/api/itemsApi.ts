09db81fceee192f3b9ddf5af467c066971c9f06d
Jas Aguilar <aguilarhans100701@gmail.com>
2024-04-20 16:53:59 +0800

    changed endpoint

--- src/api/itemsApi.ts
+++ src/api/itemsApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://pinoycart-server.vercel.app/api/v1";
 
 export const addItemApi = async (items: any) => {
 	const token = localStorage.getItem("token");
